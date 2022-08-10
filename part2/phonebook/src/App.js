61e87ac1ec1495ce5f416a17c27261ba8e2da660
Dieu Vu <dieu.vu@metropolia.fi>
2022-08-10 14:47:56 +0300

    finish 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,22 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import PersonList from './components/PersonList';
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([]);
+  const [listToShow, setListToShow] = useState(persons);
 
-  const [listToShow, setListToShow] = useState(persons)
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons')
+      .then(response => {
+        console.log(response.data);
+        setPersons(response.data);
+        setListToShow(persons);
+      })
+  }, []);
 
   return (
     <div>
